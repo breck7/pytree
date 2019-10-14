f4207c25211ad0bc7b789197bf851180eefaa86d
Xun Zhu <zhuxun2@gmail.com>
2019-10-13 23:33:30 -1000

    added more functions

--- pytree/pytree_new.py
+++ pytree/pytree_new.py
@@ -1,9 +1,8 @@
-"""
-Make a direct mapping between S-expressions and a TN
+"""Make a direct mapping between S-expressions and a TN
 
 ((word0 word1 ... ) (...child0...) (...child1...) ...)
-
 """
+import pandas as pd
 
 EDGE_CHAR = ' '
 WORD_BREAK = ' '
@@ -43,36 +42,100 @@ def get_blocks(lines):
 
 
 def str_to_tn(str_):
+    """Fully parses str_.
+
+    A full parse might not be necessary. Both blocks and the nodes can be (and
+    probably should be) parsed lazily.
+    """
     lines = str_.split(NODE_BREAK)
     return list(get_blocks(lines))
 
 
 def tn_to_lines(tn, lvl=0):
     lines = []
-    for node, *block in tn:
+    for node, *blocks in tn:
         lines.append(EDGE_CHAR * lvl + WORD_BREAK.join(node))
-        lines += tn_to_lines(block, lvl + 1)
+        lines += tn_to_lines(blocks, lvl + 1)
     return lines
 
 
-def tn_to_str(tn, lvl=0):
+def to_str(tn):
     return '\n'.join(tn_to_lines(tn))
+
+
+def count_n_nodes(tn):
+    return sum(1 + count_n_nodes(blocks) for _, *blocks in tn)
+
+
+def get_node_in(tn, indices):
+    blocks = tn
+    for index in indices:
+        node, *blocks = blocks[index]
+    return node
+
+
+def reverse(tn):
+    """Reverse the root level.
+
+    I don't think reversing the entire TN recursively is practically useful.
+    Even if it were, one could easily implement it.
+    """
+    return reversed(tn)
+
+
+def count_n_words(tn):
+    return sum(len(node) + count_n_words(blocks) for node, *blocks in tn)
+
+
+def tn_to_dict(tn):
+    """Turns a type of TN into a dict.
+
+    Example:
+
+    foo 1
+    bar 2
+
+    gets turned into
+
+    {
+      "foo": "1",
+      "bar": "2",
+    }
+
+    """
+    d = {}
+    for node, *_ in tn:
+        k, *v = node
+        d[k] = WORD_BREAK.join(v)
+    return d
+
+
+def records_to_df(tn):
+    records = []
+    for index, *record_tn in tn:
+        records.append(tn_to_dict(record_tn))
+
+    return pd.DataFrame.from_records(records)
+
+
+def records_to_csv(tn, index=False):
+    return records_to_df(tn).to_csv(index=index)
 
 
 # to pass the swim test
 # x.to_string()
-# x.get_n_lines()
-# x.as_rows_to_csv() -> may throw errors
-# pytree.csv_to_rows(csv_str)
+# x.count_n_nodes()
+# x.get_n_words()
 # x.get(0, 0, 0)
 # x.set((0, 0, 0), 1, 'aloha')
 # reverse: x.get(0, 0, 0).root = x.get(0, 0, 0).root.reverse
-# x.get_n_words()
 # x.count_word('hello')
 # pytree.from_json(json_obj) # dict, array, and str only
+# pytree.csv_to_rows(csv_str)
+# x.as_rows_to_csv() -> may throw errors
 
 # x.to_json()
-# x.get_n_nodes()
+# x.count_n_nodes()
 
 
 def test():
@@ -87,29 +150,26 @@ g
    j"""
 
     answer = [
-        [],
         [
+            ['a', 'b'],
             [
-                ['a', 'b'],
+                ['c', 'd'],
                 [
-                    ['c', 'd'],
-                    [
-                        ['e'],
-                    ],
-                    [
-                        ['f'],
-                    ],
+                    ['e'],
+                ],
+                [
+                    ['f'],
                 ],
             ],
+        ],
+        [
+            ['g'],
             [
-                ['g'],
+                ['', '', 'h'],
                 [
-                    ['', '', 'h'],
+                    ['', 'i'],
                     [
-                        ['', 'i'],
-                        [
-                            ['j'],
-                        ],
+                        ['j'],
                     ],
                 ],
             ],
@@ -123,7 +183,22 @@ g
 
     print(str(result) == str(answer))
 
-    print(tn_to_str(result))
+    print(to_str(result))
+    print(count_n_nodes(answer))  # 8
+    print(count_n_words(answer))  # 13
+    print(get_node_in(answer, [0, 0, 1]))
+    tn2_str = """\
+0
+ foo 1
+ bar 2
+1
+ foo 41
+ bar 42
+2
+ x 12"""
+    tn2 = str_to_tn(tn2_str)
+    print(tn2)
+    print(records_to_csv(tn2))
 
 
 if __name__ == '__main__':
